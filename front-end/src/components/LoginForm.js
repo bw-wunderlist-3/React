d357dc0a87b112a4095765042d7cd2704f8f3f68
Eric Rice <ericmichaelrice@protonmail.com>
2020-09-22 10:41:19 -0700

    added user authentication to login form

--- front-end/src/components/LoginForm.js
+++ front-end/src/components/LoginForm.js
@@ -1,24 +1,55 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { useForm } from 'react-hook-form';
+import {axiosWithAuth} from '../utils/axiosWithAuth'
+import {useHistory} from 'react-router-dom'
 
 const LoginForm = () => {
-  const { register, handleSubmit, errors } = useForm();
-  const onSubmit = data => console.log(data);
+
+  const [credentials, setCredentials] = useState({
+    username: '',
+    password: ''
+  })
+
+  const history = useHistory()
+
+  const handleChanges = e => {
+    e.persist()
+    setCredentials({
+      ...credentials,
+     [e.target.name]: e.target.value
+    })
+  }
+
+  const login = e => {
+    e.preventDefault()
+    axiosWithAuth()
+    .post('/login', credentials)
+    .then(res => {
+      localStorage.setItem('token', res.data.payload)
+      history.push('/protected')
+    })
+    .catch(err => {
+      console.log('Login form error catch: ', err)
+    })
+  }
+
+  const { register, errors } = useForm();
+  
   
   return (
     <div className='form-contiainer'>
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <label>Username:</label>
+        <form onSubmit={login}>
+            <label htmlFor='username'>Username:</label>
             <br></br>
-            <input type="text" placeholder="Username" name="username" ref={register({required: true, min: 5})} />
+            <input type="text" placeholder="Username" name="username" value={credentials.username} ref={register({required: true, min: 5})} />
             <br></br>
             {errors.username && 'Username must be longer than 5 characters'}
 
 
             <br></br>
-            <label>Password:</label>
+            <label htmlFor='password'>Password:</label>
             <br></br>
-            <input type="password" placeholder="Password" name="password" ref={register({required: true, min: 5})} />
+            <input type="password" placeholder="Password" name="password" value={credentials.password} ref={register({required: true, min: 5})} />
             <br></br>
             {errors.password && 'Password must be longer than 5 characters'}
             
