2957cec56a2b815cd45064ba171bd81b2f171355
Sam Lalli <2sam.lalli@gmail.com>
2020-09-22 10:21:43 -0600

    finished

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.css';
 import LoginForm from './components/LoginForm';
-import RegisterForm from './components/RegisterForm';
 
 function App() {
   return (
     <div className="App">
-      <RegisterForm />
 
     </div>
   );
