3983809d9cf708b14eb92229d6d118b4d9ea4db5
Sam Lalli <2sam.lalli@gmail.com>
2020-09-23 18:16:35 -0600

    styles added for both forms

--- front-end/src/components/LoginForm.js
+++ front-end/src/components/LoginForm.js
@@ -5,79 +5,137 @@ import {useHistory, Link} from 'react-router-dom'
 import styled from 'styled-components'
 
 
+//styles
+
+const Flex = styled.div`
+display: flex;
+justify-content:center;
+align-items: center;
+background-color: oldlace;
+color: #f44e31;
+`
+
+
 const StyledForm = styled.div`
+ @media (min-width: 1000px) {
+            width: 500px;
+ }
 border: 2px solid black;
 width: 30%;
+background-color: #E9E9E9;
+margin-top:17%;
+margin-bottom:17%;
+
+.form-header{
+    background-color: #594A4E;
+    padding:1%;
+    border-bottom: 2px solid black;
+    color:white;
+} 
+.form-footer{
+    background-color: #594A4E;
+    padding:1%;
+    border-top: 2px solid black;
+    color:white;
+}
 
 .formInputs{
     display:flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
+    color:black;
+    margin:2%;
+    font-size: 1.2rem;
 }
 
 input{
     margin: 2%;
 }
-`
 
+button{
+    color:black;
+    background-color: #F44E31;
+    outline: none;
+    border: 1px solid black;
+    padding:1%;
+    width:15%;
+    border-radius:10%;
+    transition: ease-in-out .5s;
+
+    &:hover{
+        color:#F44E31;
+        background-color: #E9E9E9;
+        transition: ease-in-out .5s;
+    }
+}
+
+.link{
+    color:#F44E31;
+}
+`
+//styles end
 
 const LoginForm = () => {
 
-  const [credentials, setCredentials] = useState({
-    username: '',
-    password: ''
-  })
-
-  const history = useHistory()
-
-  const handleChanges = e => {
-    e.persist()
-    setCredentials({
-      ...credentials,
-     [e.target.name]: e.target.value
+    const [credentials, setCredentials] = useState({
+        username: '',
+        password: ''
     })
-  }
 
-  const login = e => {
-    e.preventDefault()
-    axiosWithAuth()
-    .post('/users/login', credentials)
-    .then(res => {
-      localStorage.setItem('token', res.data.payload)
-      history.push('/protected')
-    })
-    .catch(err => {
-      console.log('Login form error catch: ', err)
-    })
-  }
+    const history = useHistory()
 
-  const { register, errors } = useForm();
+    const handleChanges = e => {
+        e.persist()
+        setCredentials({
+        ...credentials,
+        [e.target.name]: e.target.value
+        })
+    }
+
+    const login = e => {
+        e.preventDefault()
+        axiosWithAuth()
+        .post('/users/login', credentials)
+        .then(res => {
+        localStorage.setItem('token', res.data.payload)
+        history.push('/protected')
+        })
+        .catch(err => {
+        console.log('Login form error catch: ', err)
+        })
+    }
+
+    const { register, errors } = useForm();
   
   
-  return (
-    <StyledForm className='form-contiainer' >
-        <h1>Sign In To Your Account</h1>
-        <form onSubmit={login}>
-            <div className="formInputs">
-                <label htmlFor='username'>Username:</label>
-                <input type="text" placeholder="Username" name="username" value={credentials.username} ref={register({required: true, min: 5})} onChange={handleChanges} />
-                {errors.username && 'Username must be longer than 5 characters'}
-                {console.log(credentials)}
-                
+    return (
+        <Flex>
+            <StyledForm className='form-contiainer' >
+                <div className="form-header">
+                    <h1>Sign In To Your Account</h1>
+                </div>
+                <form onSubmit={login}>
+                    <div className="formInputs">
+                        <label htmlFor='username'>Username:</label>
+                        <input type="text" placeholder="Username" name="username" value={credentials.username} ref={register({required: true, min: 5})} onChange={handleChanges} />
+                        {errors.username && 'Username must be longer than 5 characters'}
+                        {console.log(credentials)}
+                        
 
-                <label htmlFor='password'>Password:</label>
-                <input type="password" placeholder="Password" name="password" value={credentials.password} ref={register({required: true, min: 5})} onChange={handleChanges} />
-                {errors.password && 'Password must be longer than 5 characters'}
-                
-                <button>{login}</button>
-            </div>
-            <div>
-                <p>Don't have an account? <Link>Sign up Here</Link> </p>
-            </div>
-        </form>
-    </StyledForm>
-  );
+                        <label htmlFor='password'>Password:</label>
+                        <input type="password" placeholder="Password" name="password" value={credentials.password} ref={register({required: true, min: 5})} onChange={handleChanges} />
+                        {errors.password && 'Password must be longer than 5 characters'}
+                        
+                        <button>Login{login}</button>
+                    </div>
+                    <div className='form-footer'>
+                        <p>Don't have an account? <Link className="link">Sign up Here</Link> </p>
+                    </div>
+                </form>
+            </StyledForm>
+        </Flex>
+    );
 }
 
 export default LoginForm
--- front-end/src/components/RegisterForm.js
+++ front-end/src/components/RegisterForm.js
@@ -1,13 +1,43 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { useForm } from 'react-hook-form';
 import styled from 'styled-components'
+import axios from 'axios'
+
+const Flex = styled.div`
+display:flex;
+justify-content: center;
+align-items:center;
+background-color: oldlace;
+color:#F44E31;
+`
 
 const StyledFormContainer = styled.div`
+ @media (min-width: 1000px) {
+            width: 500px;
+ }
 border: 2px solid black;
-width:40%;
+width: 30%;
+background-color: #E9E9E9;
+margin-top:19%;
+margin-bottom:20%;
+
+.form-header{
+    background-color:#594a4e;
+    color: white;
+    padding:1%;
+    border-bottom: 2px solid black;
+}
+
+.form-footer{
+    background-color:#594a4e;
+    padding:1%;
+    border-top: 2px solid black;
+}
 
 label{
     margin-top:2%;
+    color: black;
+    margin-bottom:1%;
 
 }
 input{
@@ -20,13 +50,28 @@ input{
     flex-direction: column;
     justify-items:center;
     align-items: center;
+    background-color:#e9e9e9;
 }
 .terms{
     width:10%;
 }
 .submit{
     width: 20%;
+    background-color:#F44E31;
+    border:1px solid black;
+    border-radius: 10%;
+    padding: 1%;
+    margin: 1%;
+    transition: ease-in-out .5;
+
+    &:hover{
+        background-color: #e9e9e9;
+        color: #F44E31;
+        transition: ease-in-out .5s;
+    }
 }
+
+
 `
 
 
@@ -63,9 +108,11 @@ const RegisterForm = () => {
     }
   
     return (
-        <>
+        <Flex>
         <StyledFormContainer className='register-form-container'>
+            <div className="form-header">
             <h1>Join Us Today</h1>
+                </div>
             <form className='form' onSubmit={handleSubmit(onSubmit)}>
 
                 <div className="userInputs">
@@ -80,31 +127,31 @@ const RegisterForm = () => {
                     {errors.email && 'Please enter a vaild email'}
                 </div>
 
-                <div className='termsBox'>
                     <label>Terms and Conditions:</label>
                     <input type="checkbox" name="terms" className='terms' ref={register({required: true})} />
                     <br></br>
                     {errors.terms && 'You must agree to the terms and conditions'}
-                </div>
+                <div className="form-footer">
                 <input type="submit" className='submit' />
+                </div>
             </form>
         </StyledFormContainer>
 
-        <div>
+        {/* <div>
             {
                 registerForm.map((user, index) =>{
                     const {username, password} = user;
                     return(
                         <div>
                             <h2>Username: {username}</h2>
-                            <p>Password Length: {password.length}</p>
+                            <p>Password Length: {password}</p>
                         </div>
                     )
 
                 })
             }
-        </div>
-        </>
+        </div> */}
+        </Flex>
     );
 }
 
